3765b561a51cd16b273aabede90b35b9c3c70697
Demerak <alexandre_poirier@outlook.com>
2021-07-23 09:55:25 -0400

    finish counter app

--- JavaScript/Counter/index.js
+++ JavaScript/Counter/index.js
@@ -1,7 +1,18 @@
 
-let count = 0
+let countEl = document.getElementById("count-el");
+let count = 0;
 
 function increment() {
-    count = count + 1
-    console.log(count)
+    count += 1;
+    countEl.textContent = count;
+    console.log(count);
+}
+
+function save() {
+    let saveValue = count + " - ";
+    let saveEl = document.getElementById("save-el");
+    saveEl.textContent += saveValue;
+    console.log("count reset"); 
+    countEl.textContent = 0;
+    count = 0;
 }
